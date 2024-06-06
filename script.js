81908ac9e5af0af03a4cb8235a6c2c2746babd10
Sandeep Rishi <123352611+18Sandeep18@users.noreply.github.com>
2024-06-06 16:06:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ const btnHold = document.querySelector('.btn--hold');
 
 let a = promt("Player 1...Enter Your name");
 player0El.textContent = a;
+let b = promt("Player 2...Enter Your name");
 player1El.textContent = b;
-let a = promt("Player 2...Enter Your name");
 
 let scores, currentScore, activePlayer, playing;
 
